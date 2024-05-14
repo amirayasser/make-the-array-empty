62281f22da0eb072a58b7e4fca7d3650980abfd3
Amira Yasser <72684587+amirayasser@users.noreply.github.com>
2024-05-14 06:20:56 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+var nums = [2, 3, 3, 2, 2, 4, 2, 3, 4]
+
+// minimum-number-of-operations-to-make-array-empty
+
+// nums.splice(0,nums.length)
+// console.log(nums)
+
+
+// const length = nums.length
+
+// for(let i = 0; i < length; i++){
+//     nums.shift()
+// }
+
+// console.log(nums) // []
+
+// ----------------------------
+// nums.length = 0
+// console.log(nums) // []
+
+// ----------------------------
+// Reassignment to an Empty Array
+nums = []
+console.log(nums) // []
